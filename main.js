ae4af21c6f8856022a735104e11f6520f0d7e8ff
Zoe <heejeong.cho@stryx.co.kr>
2021-01-02 15:03:07 +0900

    template, computed...

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue';
-import NumberBaseball from './js/NumberBaseball'
+// import NumberBaseball from './js/NumberBaseball';
+import ResponseCheck from './js/ResponseCheck';
 
-new Vue(NumberBaseball).$mount('#root')
+new Vue(ResponseCheck).$mount('#root')
--- webpack.config.js
+++ webpack.config.js
@@ -13,18 +13,25 @@ module.exports = {
     entry: {
         app: path.join(__dirname, 'main.js'), // 하나로 뭉쳐진 파일의 이름을 app에 설정해준다.
     },
-    module: {
+    module: { // javascript가 아닌 애들은 js로 바꿔준다.
         // webpack의 핵심 - 어떻게 합칠 건지, 처리할 건지를 설정한다.
         rules: [{
             test: /\.vue$/,
-            loader: 'vue-loader'
+            use: 'vue-loader'
+        }, {
+            test: /\.css$/,
+            use: [
+                'vue-style-loader',
+                'css-loader',
+            ]
         }]
     },
-    plugins: [
+    plugins: [ // output으로 나오기 전에 처리해야 할 것들을 설정해준다. (부가적)
         new VueLoaderPlugin(),
     ],
     output: {
         filename: '[name].js', // app에 들어간 파일명을 자동으로 넣어주기
         path: path.join(__dirname, 'dist'), // 폴더 경로, 현재 위치(dirname)과 합쳐준다.
+        publicPath: '/dist/', // webpack-dev-server 실행하려면 
     },
 }
